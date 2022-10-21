01f815acd9ea0edba741fb896b6bdb22471fc7bf
barathbalasubramanian <barathkumar.b2411@gmail.com>
2022-10-21 14:23:22 +0530

    initial

---
+++ main.js
@@ -0,0 +1,26 @@
+
+let dropdown1  = document.querySelector('.dropdown1')
+let dropdown2  = document.querySelector('.dropdown2')
+let dropdown3  = document.querySelector('.dropdown3')
+let dropdown4  = document.querySelector('.dropdown4')
+
+dropdown1.addEventListener( 'click' , function() { 
+    let d = document.querySelector('.down1')
+    d.classList.toggle('downn') 
+})
+
+dropdown2.addEventListener( 'click' , function() { 
+    let d = document.querySelector('.down2')
+    d.classList.toggle('downn') 
+})
+
+dropdown3.addEventListener( 'click' , function() { 
+    let d = document.querySelector('.down3')
+    d.classList.toggle('downn') 
+})
+
+dropdown4.addEventListener( 'click' , function() { 
+    let d = document.querySelector('.down4')
+    d.classList.toggle('downn') 
+})
+
